b7a457d4c58af23c78ac87f696920076d6113abb
appko2023 <ryogo5.0304@gmail.com>
2023-09-29 16:08:51 +0900

    modaljs done

--- public/js/script.js
+++ public/js/script.js
@@ -21,25 +21,93 @@ const splide = new Splide(".splide", options);
 splide.mount(window.splide.Extensions);
 
 const icebreaker = document.getElementById("icebreaker");
-const modal = document.getElementById("modal-icebreaker");
-const iframe = document.querySelector("iframe");
+const tequilaSunrise = document.getElementById("tequila-sunrise");
+const elDiablo = document.getElementById("el-diablo");
+const paloma = document.getElementById("paloma");
+const broadwayThirst = document.getElementById("broadway-thirst");
+const matador = document.getElementById("matador");
+const margarita = document.getElementById("margarita");
+const mockingbird = document.getElementById("mockingbird");
 
-window.addEventListener("load", () => {
-  console.log(icebreaker);
-});
+const modalIcebreaker = document.getElementById("modal-icebreaker");
+const modalTequilaSunrise = document.getElementById("modal-tequila-sunrise");
+const modalElDiablo = document.getElementById("modal-el-diablo");
+const modalPaloma = document.getElementById("modal-paloma");
+const modalBroadwayThirst = document.getElementById("modal-broadway-thirst");
+const modalMatador = document.getElementById("modal-matador");
+const modalMargarita = document.getElementById("modal-margarita");
+const modalMockingbird = document.getElementById("modal-mockingbird");
 
-function icebreakerOpen() {
-  modal.classList.add("is-show");
+var clickSlide;
+
+const close = document.getElementById("close");
+
+function clickClose() {
+  close.classList.remove("is-show");
 }
 
 splide.on("click", function (e) {
-  var clickSlide = e.slide.firstElementChild;
-  console.log(clickSlide);
-  if (!e.target.closest(iframe)) {
-    console.log(click);
+  clickSlide = e.slide.firstElementChild;
+  close.classList.add("is-show");
+
+  switch (clickSlide.getAttribute("id")) {
+    case icebreaker.getAttribute("id"):
+      modalIcebreaker.classList.add("is-show");
+      break;
+    case tequilaSunrise.getAttribute("id"):
+      modalTequilaSunrise.classList.add("is-show");
+      break;
+    case elDiablo.getAttribute("id"):
+      modalElDiablo.classList.add("is-show");
+      break;
+    case paloma.getAttribute("id"):
+      modalPaloma.classList.add("is-show");
+      break;
+    case broadwayThirst.getAttribute("id"):
+      modalBroadwayThirst.classList.add("is-show");
+      break;
+    case matador.getAttribute("id"):
+      modalMatador.classList.add("is-show");
+      break;
+    case margarita.getAttribute("id"):
+      modalMatador.classList.add("is-show");
+      break;
+    case mockingbird.getAttribute("id"):
+      modalMockingbird.classList.add("is-show");
+      break;
+    default:
+      break;
   }
-  if (clickSlide.getAttribute("id") == icebreaker.getAttribute("id")) {
-    console.log("成功");
-    icebreakerOpen();
+});
+
+close.addEventListener("click", (event) => {
+  clickClose();
+  switch (clickSlide.getAttribute("id")) {
+    case icebreaker.getAttribute("id"):
+      modalIcebreaker.classList.remove("is-show");
+      break;
+    case tequilaSunrise.getAttribute("id"):
+      modalTequilaSunrise.classList.remove("is-show");
+      break;
+    case elDiablo.getAttribute("id"):
+      modalElDiablo.classList.remove("is-show");
+      break;
+    case paloma.getAttribute("id"):
+      modalPaloma.classList.remove("is-show");
+      break;
+    case broadwayThirst.getAttribute("id"):
+      modalBroadwayThirst.classList.remove("is-show");
+      break;
+    case matador.getAttribute("id"):
+      modalMatador.classList.remove("is-show");
+      break;
+    case margarita.getAttribute("id"):
+      modalMatador.classList.remove("is-show");
+      break;
+    case mockingbird.getAttribute("id"):
+      modalMockingbird.classList.remove("is-show");
+      break;
+    default:
+      break;
   }
 });
