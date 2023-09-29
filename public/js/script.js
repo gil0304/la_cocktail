730ec8bf900b924c0d9768b80c50cb496c81c513
appko2023 <ryogo5.0304@gmail.com>
2023-09-29 14:21:04 +0900

    add icebreaker.html

--- public/js/script.js
+++ public/js/script.js
@@ -21,18 +21,25 @@ const splide = new Splide(".splide", options);
 splide.mount(window.splide.Extensions);
 
 const icebreaker = document.getElementById("icebreaker");
-let modal = document.getElementById("modal");
+const modal = document.getElementById("modal-icebreaker");
+const iframe = document.querySelector("iframe");
 
 window.addEventListener("load", () => {
   console.log(icebreaker);
 });
 
-function modalOpen() {
+function icebreakerOpen() {
   modal.classList.add("is-show");
 }
 
-icebreaker.addEventListener("click", modalOpen);
-
-splide.on("click", function () {
-  modalOpen();
+splide.on("click", function (e) {
+  var clickSlide = e.slide.firstElementChild;
+  console.log(clickSlide);
+  if (!e.target.closest(iframe)) {
+    console.log(click);
+  }
+  if (clickSlide.getAttribute("id") == icebreaker.getAttribute("id")) {
+    console.log("成功");
+    icebreakerOpen();
+  }
 });
