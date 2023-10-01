4f040acde8bf6d50a8f6f40323fc4e70aba71015
gil0304 <ryogo5.0304@gmail.com>
2023-10-01 16:15:17 +0900

    renew & fixend

--- public/js/script.js
+++ public/js/script.js
@@ -20,98 +20,23 @@ const options = {
 const splide = new Splide(".splide", options);
 splide.mount(window.splide.Extensions);
 
-const icebreaker = document.getElementById("icebreaker");
-const tequilaSunrise = document.getElementById("tequila-sunrise");
-const elDiablo = document.getElementById("el-diablo");
-const paloma = document.getElementById("paloma");
-const broadwayThirst = document.getElementById("broadway-thirst");
-const matador = document.getElementById("matador");
-const margarita = document.getElementById("margarita");
-const mockingbird = document.getElementById("mockingbird");
+const modal = document.getElementById("modal");
+const modalContent = document.getElementById("modal-content");
+document.getElementById("modal-bg").onclick = () => {
+  document.body.style.overflow = "visible";
+  modal.classList.remove("visible");
+};
 
-const modalIcebreaker = document.getElementById("modal-icebreaker");
-const modalTequilaSunrise = document.getElementById("modal-tequila-sunrise");
-const modalElDiablo = document.getElementById("modal-el-diablo");
-const modalPaloma = document.getElementById("modal-paloma");
-const modalBroadwayThirst = document.getElementById("modal-broadway-thirst");
-const modalMatador = document.getElementById("modal-matador");
-const modalMargarita = document.getElementById("modal-margarita");
-const modalMockingbird = document.getElementById("modal-mockingbird");
-
-var clickSlide;
-
-const mask = document.getElementById("mask");
-
-function clickMask() {
-  if (mask.classList.contains("is-show")) {
-    mask.classList.remove("is-show");
-  } else {
-    mask.classList.add("is-show");
-  }
-}
-
-splide.on("click", function (e) {
-  clickSlide = e.slide.firstElementChild;
-  clickMask();
-
-  switch (clickSlide.getAttribute("id")) {
-    case icebreaker.getAttribute("id"):
-      modalIcebreaker.classList.add("is-show");
-      break;
-    case tequilaSunrise.getAttribute("id"):
-      modalTequilaSunrise.classList.add("is-show");
-      break;
-    case elDiablo.getAttribute("id"):
-      modalElDiablo.classList.add("is-show");
-      break;
-    case paloma.getAttribute("id"):
-      modalPaloma.classList.add("is-show");
-      break;
-    case broadwayThirst.getAttribute("id"):
-      modalBroadwayThirst.classList.add("is-show");
-      break;
-    case matador.getAttribute("id"):
-      modalMatador.classList.add("is-show");
-      break;
-    case margarita.getAttribute("id"):
-      modalMargarita.classList.add("is-show");
-      break;
-    case mockingbird.getAttribute("id"):
-      modalMockingbird.classList.add("is-show");
-      break;
-    default:
-      break;
-  }
-});
-
-mask.addEventListener("click", (event) => {
-  clickMask();
-  switch (clickSlide.getAttribute("id")) {
-    case icebreaker.getAttribute("id"):
-      modalIcebreaker.classList.remove("is-show");
-      break;
-    case tequilaSunrise.getAttribute("id"):
-      modalTequilaSunrise.classList.remove("is-show");
-      break;
-    case elDiablo.getAttribute("id"):
-      modalElDiablo.classList.remove("is-show");
-      break;
-    case paloma.getAttribute("id"):
-      modalPaloma.classList.remove("is-show");
-      break;
-    case broadwayThirst.getAttribute("id"):
-      modalBroadwayThirst.classList.remove("is-show");
-      break;
-    case matador.getAttribute("id"):
-      modalMatador.classList.remove("is-show");
-      break;
-    case margarita.getAttribute("id"):
-      modalMargarita.classList.remove("is-show");
-      break;
-    case mockingbird.getAttribute("id"):
-      modalMockingbird.classList.remove("is-show");
-      break;
-    default:
-      break;
-  }
+Array.from(document.getElementsByClassName("glass-img")).forEach((ele) => {
+  ele.onclick = () => {
+    if (document.body.clientWidth > 450) {
+      document.body.style.overflow = "hidden";
+      modalContent.setAttribute("src", `pages/${ele.dataset.name}.html`);
+      modalContent.onload = () => {
+        modal.classList.add("visible");
+      };
+    } else {
+      window.location.href = `/pages/${ele.dataset.name}.html`;
+    }
+  };
 });
